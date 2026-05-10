c70d356d89d49d1dec8d6aae10ca7b342869d75a
MaryamAshraf4 <maryamashraf386@gmail.com>
2026-05-10 20:55:53 +0300

    Add HeatDiffusion implementation and header

--- AdvancedParallelGridMPI.cpp
+++ AdvancedParallelGridMPI.cpp
@@ -1,29 +1,58 @@
 #include <mpi.h>
-#include <stdio.h>
 #include <iostream>
+#include <string>
 
+#include "HeatDiffusion.h"
+//#include "MatrixMultiplication.h"
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[])
+{
     MPI_Init(&argc, &argv);
-    int rank, size, token;
+
+    int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-    if (rank == 0) {
-        token = 5;
-        MPI_Send(&token, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
-        MPI_Recv(&token, 1, MPI_INT, size - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        cout << "Process 0 received " << token << endl;
-    }
-    else {
-        MPI_Recv(&token, 1, MPI_INT, rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        token++;
-        MPI_Send(&token, 1, MPI_INT, (rank + 1) % size, 0, MPI_COMM_WORLD);
 
+    if (size < 2)
+    {
+        if (rank == 0)
+            cout << "Run with at least 2 processes.\n";
+
+        MPI_Finalize();
+        return 0;
     }
+
+    string choice;
+
+    if (rank == 0)
+    {
+        cout << "Choose Algorithm:\n";
+        cout << "1. Heat Diffusion\n";
+        cout << "2. Matrix Multiplication\n";
+        cin >> choice;
+    }
+
+    char algo[20];
+
+    if (rank == 0)
+        strcpy_s(algo, choice.c_str());
+
+    MPI_Bcast(algo, 20, MPI_CHAR, 0, MPI_COMM_WORLD);
+
+    if (string(algo) == "1")
+        RunHeatDiffusion(rank, size);
+
+    //else if (string(algo) == "2")
+    //    RunMatrixMultiplication(rank, size);
+
+    else
+    {
+        if (rank == 0)
+            cout << "Invalid Choice.\n";
+    }
+
     MPI_Finalize();
-
-
-
+    return 0;
 }
