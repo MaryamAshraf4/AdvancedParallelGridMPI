88f3c4035eaf55016f8e86c24cae3e96601bc77c
HajerMabrouk11 <hmabrouk476@gmail.com>
2026-05-11 15:26:55 +0300

    edit in heatdiffusion_main

--- HeatDiffusion.cpp
+++ HeatDiffusion.cpp
@@ -62,16 +62,16 @@ Config parse_args(int argc, char** argv)
         else if (a == "--no-save")     
             cfg.save_output = false;
 
-    /*    else if (a == "--demo-deadlock")    
-            cfg.demo_deadlock = true;*/
+        else if (a == "--demo-deadlock")    
+            cfg.demo_deadlock = true;
 
-     /*   else if (a == "--mode" && i+1 < argc) {
+        else if (a == "--mode" && i+1 < argc) {
             string m = argv[++i];
             if (m == "blocking")  
                 cfg.mode = MODE_BLOCKING;
             else if (m == "nonblocking") 
                 cfg.mode = MODE_NONBLOCKING;
-        }*/
+        }
     }
     return cfg;
 }
@@ -440,9 +440,6 @@ void run_solver(const Config& cfg, int rank, int size, MPI_Comm compute_comm)
     }
 }
 
-/* ═════════════════════════════════════════════════════════════
-   BENCHMARK RUNNER  (vary process count analysis)
-   ═════════════════════════════════════════════════════════════ */
 void print_performance_header(int rank)
 {
     if (rank != 0) return;
@@ -450,9 +447,7 @@ void print_performance_header(int rank)
     ruler(70);
 }
 
-/* ═════════════════════════════════════════════════════════════
-   MAIN
-   ═════════════════════════════════════════════════════════════ */
+
 void run_heat_diffusion(int argc, char** argv, int comm_choice)
 {
     int world_rank, world_size;
@@ -466,7 +461,7 @@ void run_heat_diffusion(int argc, char** argv, int comm_choice)
     }
 
     Config cfg = parse_args(argc, argv);
-    cfg.mode = MODE_NONBLOCKING; // default safe value
+    cfg.mode = MODE_NONBLOCKING; 
     if (comm_choice == 1)
         cfg.mode = MODE_BLOCKING;
     else if (comm_choice == 2)
@@ -476,43 +471,8 @@ void run_heat_diffusion(int argc, char** argv, int comm_choice)
         cfg.mode = MODE_DEADLOCK_DEMO;
         cfg.demo_deadlock = true;
     }
-    // int compute_color = 0;
-    // //int monitor_color = (world_rank == 0) ? 1 : MPI_UNDEFINED;
-
-    // MPI_Comm compute_comm;
-
-    // MPI_Comm_split(MPI_COMM_WORLD, compute_color, world_rank, &compute_comm);
-    // //MPI_Comm_split(MPI_COMM_WORLD, monitor_color, world_rank, &monitor_comm);
-
-    // int compute_rank, compute_size;
-
-    // MPI_Comm_rank(compute_comm, &compute_rank);
-    // MPI_Comm_size(compute_comm, &compute_size);
-
-    // if (compute_rank == 0) {
-    //     ruler();
-    //     cout << " Heat Diffusion MPI Solver\n";
-    //     ruler();
-    // }
-
-    ///* if (cfg.demo_deadlock) {
-    //     demo_deadlock_scenario(compute_comm, compute_rank, compute_size, cfg.cols);
-    //     MPI_Barrier(compute_comm);
-    // }*/
-
-    // print_performance_header(compute_rank);
-
-    // run_solver(cfg, world_rank, world_size, compute_comm);
-    // //run_solver(cfg, world_rank, world_size, compute_comm, monitor_com);
-
-    // MPI_Comm_free(&compute_comm);
-
-    // //if (monitor_comm != MPI_COMM_NULL)
-    // //    MPI_Comm_free(&monitor_comm);
+    
     int color;
-
-    /* Rank 0 = monitor process
-    باقي الـ ranks = compute processes */
 
     if (world_rank == 0)
         color = 1;
@@ -531,7 +491,6 @@ void run_heat_diffusion(int argc, char** argv, int comm_choice)
     if (color == 0)
     {
 
-
         int compute_rank, compute_size;
 
         MPI_Comm_rank(split_comm, &compute_rank);
@@ -543,7 +502,7 @@ void run_heat_diffusion(int argc, char** argv, int comm_choice)
             ruler();
         }
 
-        // print_performance_header(compute_rank);
+         print_performance_header(compute_rank);
 
         run_solver(cfg,
             world_rank,
@@ -563,7 +522,6 @@ void run_heat_diffusion(int argc, char** argv, int comm_choice)
         cout << " Heat Diffusion MPI Solver\n";
         ruler();
 
-        print_performance_header(world_rank);
 
         cout << "[Monitor] Rank "
             << world_rank
